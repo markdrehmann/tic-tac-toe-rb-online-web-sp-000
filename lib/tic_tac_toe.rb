a3d7449dcac266044e50699bba6cb18ad7416e3b
Mark Drehmann <markdrehmann@gmail.com>
2020-01-05 21:43:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -100,3 +100,17 @@ def over?(board)
     false
   end
 end
+
+def winner(board)
+  index = []
+  index = won?(board)
+  if !index
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "Way to be, X"
+    else
+      return "Kudos, O"
+    end
+  end
+end
