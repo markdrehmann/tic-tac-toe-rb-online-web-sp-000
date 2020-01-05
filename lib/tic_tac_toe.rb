36edc139d27e6cfc38cad81397f64e3c5b2c008f
Mark Drehmann <markdrehmann@gmail.com>
2020-01-05 21:28:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,3 +70,22 @@ def current_player(board)
     return "O"
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    board[win_combo[0]] == board[win_combo[1]] == board[win_combo[2]] &&
+    position_taken?(board, win_combo[0])
+  end
+end
+
+def full?(board)
+
+end
+
+def draw?(board)
+
+end
+
+def over?(board)
+
+end
